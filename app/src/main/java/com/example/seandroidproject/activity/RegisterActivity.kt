67fefa6cda2a5766221d71baea6672ab532df372
kirtik_jangale <kirtik.jangale2000@gmail.com>
2020-08-22 14:22:02 +0530

    register screen updated

--- app/src/main/java/com/example/seandroidproject/activity/RegisterActivity.kt
+++ app/src/main/java/com/example/seandroidproject/activity/RegisterActivity.kt
@@ -13,7 +13,7 @@ import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
 
-    lateinit var imgUserPhoto : ImageView
+
     lateinit var etUserName : EditText
     lateinit var etUserEmail : EditText
     lateinit var etUserPhone : EditText
@@ -22,12 +22,10 @@ class RegisterActivity : AppCompatActivity() {
     lateinit var etUserPasswordConfirm : EditText
     lateinit var btnRegister : Button
 
-    private var imageData: ByteArray? = null
 
 
-    companion object {
-        private const val IMAGE_PICK_CODE = 999
-    }
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +33,7 @@ class RegisterActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        imgUserPhoto = findViewById(R.id.imgUserPhoto)
+
         etUserName = findViewById(R.id.etUserName)
         etUserEmail = findViewById(R.id.etUserEmail)
         etUserPhone = findViewById(R.id.etUserPhone)
@@ -44,37 +42,8 @@ class RegisterActivity : AppCompatActivity() {
         etUserPasswordConfirm = findViewById(R.id.etUserPasswordConfirm)
         btnRegister = findViewById(R.id.btnRegister)
 
-        imgUserPhoto.setOnClickListener{
-            launchGallery()
-        }
-
-
-
-
     }
 
-    private fun launchGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_PICK_CODE)
-    }
-    @Throws(IOException::class)
-    private fun createImageData(uri: Uri) {
-        val inputStream = contentResolver.openInputStream(uri)
-        inputStream?.buffered()?.use {
-            imageData = it.readBytes()
-        }
-    }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
-            val uri = data?.data
-            if (uri != null) {
-                imgUserPhoto.setImageURI(uri)
-                createImageData(uri)
-            }
-            println(uri)
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
+
 }
