f552c18bfde8f73c7da31349055765b18571c7ad
kirtik_jangale <kirtik.jangale2000@gmail.com>
2020-08-22 21:35:55 +0530

    Drawer Added

---
+++ app/src/main/java/com/example/seandroidproject/activity/HomePageActivity.kt
@@ -0,0 +1,12 @@
+package com.example.seandroidproject.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.seandroidproject.R
+
+class HomePageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
