779e76ce0bb82097dfdbd6d86103654ebf08bfb4
officiallynik <nikhilownstheworld@gmail.com>
2020-08-24 18:22:42 +0530

    changes to app drawer

--- app/src/main/java/com/example/seandroidproject/activity/HomePageActivity.kt
+++ app/src/main/java/com/example/seandroidproject/activity/HomePageActivity.kt
@@ -1,11 +1,15 @@
 package com.example.seandroidproject.activity
 
+import android.content.Intent
+import android.net.wifi.hotspot2.pps.HomeSp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.view.GravityCompat
@@ -13,6 +17,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.example.seandroidproject.R
 import com.example.seandroidproject.fragment.*
 import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.drawer_header_nouser.*
 
 class HomePageActivity : AppCompatActivity() {
 
@@ -24,99 +29,200 @@ class HomePageActivity : AppCompatActivity() {
 
     var previousMenuItem : MenuItem? = null
 
+    // just for testing ==> to be connected with backend
+    var isLoggedIn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
 
-        frameLayout = findViewById(R.id.frame)
-        coordinatorLayout = findViewById(R.id.coordinatorLayout)
-        toolbar = findViewById(R.id.toolbar)
-        navigationView = findViewById(R.id.navigationView)
-        drawerLayout = findViewById(R.id.drawerLayout)
+        if (isLoggedIn) {
+            setContentView(R.layout.activity_home_page)
 
-        val navigationView = findViewById<NavigationView>(R.id.navigationView)
-        val headerView = navigationView.getHeaderView(0)
-        val navUsername = headerView.findViewById<View>(R.id.txtName) as TextView
-        val navMobileNumber = headerView.findViewById<View>(R.id.txtNumber) as TextView
+            frameLayout = findViewById(R.id.frame)
+            coordinatorLayout = findViewById(R.id.coordinatorLayout)
+            toolbar = findViewById(R.id.toolbar)
+            navigationView = findViewById(R.id.navigationView)
+            drawerLayout = findViewById(R.id.drawerLayout)
 
-        setUpToolbar()
+            val navigationView = findViewById<NavigationView>(R.id.navigationView)
+            val headerView = navigationView.getHeaderView(0)
+//        val navUsername = headerView.findViewById<View>(R.id.txtName) as TextView
+//        val navMobileNumber = headerView.findViewById<View>(R.id.txtNumber) as TextView
 
-        val actionBarDrawerToggle = ActionBarDrawerToggle(this@HomePageActivity,drawerLayout,
-            R.string.open_drawer,
-            R.string.close_drawer
-        )
+            setUpToolbar()
 
-        drawerLayout.addDrawerListener(actionBarDrawerToggle)
+            val actionBarDrawerToggle = ActionBarDrawerToggle(this@HomePageActivity,drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+            )
 
-        actionBarDrawerToggle.syncState()
+            drawerLayout.addDrawerListener(actionBarDrawerToggle)
 
-        openHome()
+            actionBarDrawerToggle.syncState()
 
-        navigationView.setNavigationItemSelectedListener {
+            openHome()
 
-            if(previousMenuItem != null){
-                previousMenuItem?.isChecked = false
+            navigationView.setNavigationItemSelectedListener {
+
+                if(previousMenuItem != null){
+                    previousMenuItem?.isChecked = false
+                }
+                it.isCheckable = true
+                it.isChecked = true
+                previousMenuItem = it
+
+                when(it.itemId){
+                    R.id.home ->{
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.frame,
+                            AllItemsFragment()
+                        ).commit()
+                        drawerLayout.closeDrawers()
+                        supportActionBar?.title = "All Items"
+                    }
+                    R.id.wishList ->{
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.frame,
+                            WishlistFragment()
+                        ).commit()
+                        drawerLayout.closeDrawers()
+                        supportActionBar?.title = "Wishlist"
+                    }
+                    R.id.sell ->{
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.frame,
+                            SellItemsFragment()
+                        ).commit()
+                        drawerLayout.closeDrawers()
+                        supportActionBar?.title = "Post Item"
+                    }
+
+                    R.id.profile ->{
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.frame,
+                            ProfileFragment()
+                        ).commit()
+                        drawerLayout.closeDrawers()
+                        supportActionBar?.title = "My Profile"
+                    }
+
+                    R.id.listings ->{
+                        supportFragmentManager.beginTransaction().replace(
+                            R.id.frame,
+                            MyListingsFragment()
+                        ).commit()
+                        drawerLayout.closeDrawers()
+                        supportActionBar?.title = "My Listings"
+                    }
+
+//                R.id.drawer_logout ->{
+//                    supportFragmentManager.beginTransaction().replace(
+//                        R.id.frame,
+//                        LogoutFragment()
+//                    ).commit()
+//                    drawerLayout.closeDrawers()
+//                    supportActionBar?.title = "Logout"
+//                }
+                }
+
+                return@setNavigationItemSelectedListener true
             }
-            it.isCheckable = true
-            it.isChecked = true
-            previousMenuItem = it
-
-            when(it.itemId){
-                R.id.home ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.frame,
-                        AllItemsFragment()
-                    ).commit()
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.title = "All Items"
-                }
-                R.id.wishList ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.frame,
-                        WishlistFragment()
-                    ).commit()
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.title = "Wishlist"
-                }
-                R.id.sell ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.frame,
-                        SellItemsFragment()
-                    ).commit()
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.title = "Post Item"
-                }
-
-                R.id.profile ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.frame,
-                        ProfileFragment()
-                    ).commit()
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.title = "My Profile"
-                }
-
-                R.id.listings ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.frame,
-                        MyListingsFragment()
-                    ).commit()
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.title = "My Listings"
-                }
-
-                R.id.logout ->{
-                    supportFragmentManager.beginTransaction().replace(
-                        R.id.frame,
-                        LogoutFragment()
-                    ).commit()
-                    drawerLayout.closeDrawers()
-                    supportActionBar?.title = "Logout"
-                }
-            }
-
-            return@setNavigationItemSelectedListener true
         }
+        else {
+            setContentView(R.layout.activity_home_page_nouser)
+
+            frameLayout = findViewById(R.id.frame)
+            coordinatorLayout = findViewById(R.id.coordinatorLayout)
+            toolbar = findViewById(R.id.toolbar)
+            navigationView = findViewById(R.id.navigationView)
+            drawerLayout = findViewById(R.id.drawerLayout)
+
+            val navigationView = findViewById<NavigationView>(R.id.navigationView)
+            val headerView = navigationView.getHeaderView(0)
+    //        val navUsername = headerView.findViewById<View>(R.id.txtName) as TextView
+    //        val navMobileNumber = headerView.findViewById<View>(R.id.txtNumber) as TextView
+
+            setUpToolbar()
+
+            val actionBarDrawerToggle = ActionBarDrawerToggle(this@HomePageActivity,drawerLayout,
+                R.string.open_drawer,
+                R.string.close_drawer
+            )
+
+            drawerLayout.addDrawerListener(actionBarDrawerToggle)
+
+            actionBarDrawerToggle.syncState()
+
+            openHome()
+
+            navigationView.setNavigationItemSelectedListener {
+
+                if(previousMenuItem != null){
+                    previousMenuItem?.isChecked = false
+                }
+                it.isCheckable = true
+                it.isChecked = true
+                previousMenuItem = it
+
+                when(it.itemId){
+//                    R.id.home ->{
+//                        supportFragmentManager.beginTransaction().replace(
+//                            R.id.frame,
+//                            AllItemsFragment()
+//                        ).commit()
+//                        drawerLayout.closeDrawers()
+//                        supportActionBar?.title = "All Items"
+//                    }
+//                    R.id.wishList ->{
+//                        supportFragmentManager.beginTransaction().replace(
+//                            R.id.frame,
+//                            WishlistFragment()
+//                        ).commit()
+//                        drawerLayout.closeDrawers()
+//                        supportActionBar?.title = "Wishlist"
+//                    }
+//                    R.id.sell ->{
+//                        supportFragmentManager.beginTransaction().replace(
+//                            R.id.frame,
+//                            SellItemsFragment()
+//                        ).commit()
+//                        drawerLayout.closeDrawers()
+//                        supportActionBar?.title = "Post Item"
+//                    }
+//
+//                    R.id.profile ->{
+//                        supportFragmentManager.beginTransaction().replace(
+//                            R.id.frame,
+//                            ProfileFragment()
+//                        ).commit()
+//                        drawerLayout.closeDrawers()
+//                        supportActionBar?.title = "My Profile"
+//                    }
+//
+//                    R.id.listings ->{
+//                        supportFragmentManager.beginTransaction().replace(
+//                            R.id.frame,
+//                            MyListingsFragment()
+//                        ).commit()
+//                        drawerLayout.closeDrawers()
+//                        supportActionBar?.title = "My Listings"
+//                    }
+//
+//                    R.id.drawer_logout ->{
+//                        supportFragmentManager.beginTransaction().replace(
+//                            R.id.frame,
+//                            LogoutFragment()
+//                        ).commit()
+//                        drawerLayout.closeDrawers()
+//                        supportActionBar?.title = "Logout"
+//                    }
+                }
+
+                return@setNavigationItemSelectedListener true
+            }
+        }
+
+
 
     }
 
@@ -126,7 +232,7 @@ class HomePageActivity : AppCompatActivity() {
             AllItemsFragment()
         ).addToBackStack("Home").commit()
         drawerLayout.closeDrawers()
-        supportActionBar?.title = "All Restaurants"
+        supportActionBar?.title = "All Items"
         navigationView.setCheckedItem(R.id.home)
     }
 
